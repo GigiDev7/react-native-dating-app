ca118cf3836330ab59d142075572c678b914b3e1
GigiDev7 <gigivadachkoria@gmail.com>
2022-11-16 12:19:16 +0400

    working on tab navigation, added basic home and profile screens:

--- client/App.js
+++ client/App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
+import { Fontisto, Ionicons } from "@expo/vector-icons";
 import NotAuthScreen from "./screens/NotAuthScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Colors } from "./utils/constants";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AuthStack = () => {
   return (
@@ -24,12 +28,42 @@ const AuthStack = () => {
   );
 };
 
+const MainStack = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Fontisto name="tinder" color={color} size={size} />
+          ),
+        }}
+        name="Home"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </Tab.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <AuthStack />
+        <MainStack />
       </NavigationContainer>
     </>
   );
