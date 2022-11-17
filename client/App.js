f9040918de2aa61ed927e11019d42b603a4d3353
GigiDev7 <gigivadachkoria@gmail.com>
2022-11-17 16:59:55 +0400

    added nested navigation:

--- client/App.js
+++ client/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MessageBoxScreen from "./screens/MessageBoxScreen";
 import MainStack from "./MainStack";
 import AuthStack from "./AuthStack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <MainStack />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Main" component={MainStack} />
+          <Stack.Screen name="Messages" component={MessageBoxScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
---
+++ client/screens/MessageBoxScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const MessageBoxScreen = () => {
+  return (
+    <View>
+      <Text>message box</Text>
+    </View>
+  );
+};
+
+export default MessageBoxScreen;
