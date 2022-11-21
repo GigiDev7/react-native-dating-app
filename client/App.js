c8891a8ec92b6f376f2927be6b07f968a1d5eedd
GigiDev7 <gigivadachkoria@gmail.com>
2022-11-21 14:02:42 +0400

    added redux toolkit for state managment

--- client/App.js
+++ client/App.js
@@ -4,23 +4,43 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MessageBoxScreen from "./screens/MessageBoxScreen";
 import MainStack from "./MainStack";
 import AuthStack from "./AuthStack";
+import store from "./store";
+import { Provider, useSelector } from "react-redux";
 
 const Stack = createNativeStackNavigator();
+
+const Root = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  if (!user) {
+    return (
+      <NavigationContainer>
+        <AuthStack />
+      </NavigationContainer>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Main"
+          component={MainStack}
+        />
+        <Stack.Screen name="MessageBox" component={MessageBoxScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Main"
-            component={MainStack}
-          />
-          <Stack.Screen name="MessageBox" component={MessageBoxScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Provider store={store}>
+        <Root />
+      </Provider>
     </>
   );
 }
