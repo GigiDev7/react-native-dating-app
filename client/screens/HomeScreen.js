6abbc603bc9b255615bde170258449cde8b5e58f
GigiDev7 <gigivadachkoria@gmail.com>
2022-11-22 14:06:02 +0400

    added location services

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,11 +1,27 @@
-import { useLayoutEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { StyleSheet, View, Alert } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import FilterModal from "../components/modals/FilterModal";
 import ProfileCard from "../components/ProfileCard";
+import * as Location from "expo-location";
 
 const HomeScreen = ({ navigation }) => {
   const [isFilterModalShown, setIsFilterModalShown] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert(
+          "Insufficient permissions",
+          "You need to grant location permission to use this app properly"
+        );
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    })();
+  }, []);
 
   const showFilterModal = () => {
     setIsFilterModalShown(true);
